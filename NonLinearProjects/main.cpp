fae57e4885b60d732459d1afe679534793c68045
SplatterMelon <splattermelon@gmail.com>
2022-03-27 17:07:18 -0400

    insert works just not height

--- NonLinearProjects/main.cpp
+++ NonLinearProjects/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-	double randomN = 0.0;
+	/*double randomN = 0.0;
 
 	for (int i = 100; i < 100001; i *= 10)
 	{
@@ -42,7 +42,32 @@ int main()
 		}
 
 		cout << "\n\nNext N" << endl;
-	}
+	}*/
+
+	//RedBlackTree rbt;
+	//RBTNode* rbtRoot = new RBTNode(46);
+	//rbt.insert(rbtRoot);
+	//rbt.insert(new RBTNode(28));
+	//rbt.insert(new RBTNode(64));
+	//rbt.insert(new RBTNode(19));
+	//rbt.insert(new RBTNode(37));
+	//rbt.insert(new RBTNode(55));
+	//rbt.insert(new RBTNode(73));
+	//rbt.insert(new RBTNode(91));
+
+	//while (rbtRoot->parent != nullptr)
+	//{
+	//	rbtRoot = rbtRoot->parent;
+	//}
+
+	///*std::cout << rbtRoot->key << std::endl;
+	//std::cout << rbtRoot->left->key << " " << rbtRoot->right->key << std::endl;
+	//std::cout << rbtRoot->right->left->key << " " << rbtRoot->right->right->key << endl;
+	//std::cout << rbtRoot->left->left->key << " " << rbtRoot->left->right->key << std::endl;
+	//cout << rbtRoot->right->right->right->key;*/
+
+
+	//cout << rbtRoot->height << endl;
 
 	return 0;
 }
