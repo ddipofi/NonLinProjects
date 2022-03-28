243a70ca1512297ab87a73093b84d91c37784d41
SplatterMelon <splattermelon@gmail.com>
2022-03-28 16:26:01 -0400

    doine everything but height

--- NonLinearProjects/main.cpp
+++ NonLinearProjects/main.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 int main()
 {
-	double randomN = 0.0;
 	auto start = std::chrono::high_resolution_clock::now();
 	auto stop = start;
 	auto totalTimeAVL = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
@@ -18,22 +17,22 @@ int main()
 	{
 		for (int j = 0; j < 10; j++)
 		{
-			randomN = (double)rand() / RAND_MAX;
-			AVLTree a;
-			Node* aRoot = new Node(randomN);
-			a.insert(aRoot);
-			RedBlackTree b;
-			RBTNode* bRoot = new RBTNode(randomN);
-			b.insert(bRoot);
-			std::vector<double> randomList(i - 1);
+			std::vector<double> randomList(i);
 
-			for (int l = 0; l < i - 1; l++)
+			for (int l = 0; l < i; l++)
 			{
 				randomList.at(l) = (double)rand() / RAND_MAX;
 			}
+
+			AVLTree a;
+			Node* aRoot = new Node(randomList.at(0));
+			a.insert(aRoot);
+			RedBlackTree b;
+			RBTNode* bRoot = new RBTNode(randomList.at(0));
+			b.insert(bRoot);
 			
 			start = std::chrono::high_resolution_clock::now();
-			for (int m = 0; m < i - 1; m++)
+			for (int m = 1; m < i; m++)
 			{
 				a.insert(new Node(randomList.at(m)));
 			}
@@ -41,7 +40,7 @@ int main()
 			totalTimeAVL = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
 			start = std::chrono::high_resolution_clock::now();
-			for (int k = 0; k < i - 1; k++)
+			for (int k = 1; k < i; k++)
 			{
 				b.insert(new RBTNode(randomList.at(k)));
 			}
@@ -58,8 +57,102 @@ int main()
 				bRoot = bRoot->parent;
 			}
 
-			cout << "AVL:   " << aRoot->height << "  :  " << totalTimeAVL.count() / 10 << endl;
-			cout << "RBT:   " << bRoot->height << "  :  " << totalTimeRBT.count() / 10 << "\n" << endl;
+			cout << "AVL unsorted:   " << aRoot->height << "  :  " << totalTimeAVL.count() / 10 << endl;
+			cout << "RBT unsorted:   " << bRoot->height << "  :  " << totalTimeRBT.count() / 10 << "\n" << endl;
+		}
+
+		for (int j = 0; j < 10; j++)
+		{
+			std::vector<double> sortedList(i);
+
+			for (int l = 0; l < i; l++)
+			{
+				sortedList.at(l) = (double)rand() / RAND_MAX;
+			}
+
+			sort(sortedList.begin(), sortedList.end());
+			AVLTree a;
+			Node* aRoot = new Node(sortedList.at(0));
+			a.insert(aRoot);
+			RedBlackTree b;
+			RBTNode* bRoot = new RBTNode(sortedList.at(0));
+			b.insert(bRoot);
+
+			start = std::chrono::high_resolution_clock::now();
+			for (int m = 1; m < i; m++)
+			{
+				a.insert(new Node(sortedList.at(m)));
+			}
+			stop = std::chrono::high_resolution_clock::now();
+			totalTimeAVL = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+			start = std::chrono::high_resolution_clock::now();
+			for (int k = 1; k < i; k++)
+			{
+				b.insert(new RBTNode(sortedList.at(k)));
+			}
+			stop = std::chrono::high_resolution_clock::now();
+			totalTimeRBT = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+			while (aRoot->parent != nullptr)
+			{
+				aRoot = aRoot->parent;
+			}
+
+			while (bRoot->parent != nullptr)
+			{
+				bRoot = bRoot->parent;
+			}
+
+			cout << "AVL sorted:   " << aRoot->height << "  :  " << totalTimeAVL.count() / 10 << endl;
+			cout << "RBT sorted:   " << bRoot->height << "  :  " << totalTimeRBT.count() / 10 << "\n" << endl;
+		}
+
+		for (int j = 0; j < 10; j++)
+		{
+			std::vector<double> semiSortedList(i);
+
+			for (int l = 0; l < i; l++)
+			{
+				semiSortedList.at(l) = (double)rand() / RAND_MAX;
+			}
+
+			sort(semiSortedList.begin() + semiSortedList.size() / 4, semiSortedList.end() - semiSortedList.size() / 4);
+			AVLTree a;
+			Node* aRoot = new Node(semiSortedList.at(0));
+			a.insert(aRoot);
+			RedBlackTree b;
+			RBTNode* bRoot = new RBTNode(semiSortedList.at(0));
+			b.insert(bRoot);
+
+			start = std::chrono::high_resolution_clock::now();
+			for (int m = 1; m < i; m++)
+			{
+				a.insert(new Node(semiSortedList.at(m)));
+			}
+			stop = std::chrono::high_resolution_clock::now();
+			totalTimeAVL = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+			start = std::chrono::high_resolution_clock::now();
+			for (int k = 1; k < i; k++)
+			{
+				b.insert(new RBTNode(semiSortedList.at(k)));
+			}
+			stop = std::chrono::high_resolution_clock::now();
+			totalTimeRBT = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+			while (aRoot->parent != nullptr)
+			{
+				aRoot = aRoot->parent;
+			}
+
+			while (bRoot->parent != nullptr)
+			{
+				bRoot = bRoot->parent;
+			}
+
+			cout << "AVL semi-sorted:   " << aRoot->height << "  :  " << totalTimeAVL.count() / 10 << endl;
+			cout << "RBT semi-sorted:   " << bRoot->height << "  :  " << totalTimeRBT.count() / 10 << "\n" << endl;
 		}
 
 		cout << "\n\nNext N" << endl;
