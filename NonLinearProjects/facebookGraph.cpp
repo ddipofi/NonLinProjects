e73114917413b5baa104955cc66f812f48f7fa1d
SplatterMelon <splattermelon@gmail.com>
2022-04-11 18:03:16 -0400

    hw10

--- NonLinearProjects/facebookGraph.cpp
+++ NonLinearProjects/facebookGraph.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 unordered_map<int, vector<int>> uMap;
+vector<string> edgeValues;
 void addEdge(int vertex1, int vertex2);
 void addNewVertex(int vertex);
 void printList(unordered_map<int, vector<int>> const& m);
@@ -61,99 +62,20 @@ void printList(unordered_map<int, vector<int>> const& m)
 
 void addEdge(int vertex1, int vertex2)
 {
-    uMap[vertex1].push_back(vertex2);
-    uMap[vertex2].push_back(vertex1);
+    if (find(edgeValues.begin(), edgeValues.end(), to_string(vertex1) + ":" + (to_string(vertex2))) == edgeValues.end() && find(edgeValues.begin(), edgeValues.end(), to_string(vertex2) + ":" + (to_string(vertex1))) == edgeValues.end())
+    {
+        uMap[vertex1].push_back(vertex2);
+        uMap[vertex2].push_back(vertex1);
+        edgeValues.push_back(to_string(vertex1) + ":" + (to_string(vertex2)));
+        edgeValues.push_back(to_string(vertex2) + ":" + (to_string(vertex1)));
+    }
 }
 
-void addNewVertex(int vertex) {
-
+void addNewVertex(int vertex)
+{
     if (uMap.find(vertex) == uMap.end())
     {
         vector<int> vectorr;
         uMap.insert({vertex, vectorr});
     }
 }
-
-
-
-
-
-
-
-//#include <iostream>
-//#include <vector>
-//#include <fstream>
-//#include <string>
-//#include <unordered_map>
-//#include <forward_list>
-//
-//using namespace std;
-//
-//const string facebookText = "facebook-links.txt";
-//
-//class graph
-//{
-//public:
-//	unordered_map<int, forward_list<int>>* outerMap;
-//
-//	graph()
-//	{
-//		//outer = new vector<int>;
-//	}
-//
-//	void addEdge(int x, int y)
-//	{
-//		forward_list<int> fList;
-//
-//		outerMap.emplace(x, fList).first->second = fList;
-//		
-//		
-//
-//		outerMap[x] = fList;
-//		outer[y].push_back(x);
-//	}
-//
-//	void printAdjList()
-//	{
-//		for (int i = 1; i < 100; i++)
-//		{
-//			cout << i << ": [";
-//			for (int adj : outer[i])
-//			{
-//				cout << adj << ",";
-//			}
-//			cout << "]" << endl;
-//		}
-//	}
-//};
-//
-//int main()
-//{
-//	ifstream inFS;
-//	inFS.open(facebookText);
-//
-//	if (!inFS.is_open())
-//	{
-//		cout << "Could not open file " << facebookText << endl;
-//		return EXIT_FAILURE;
-//	}
-//
-//	graph g;
-//	int a = 0;
-//	int b = 0;
-//	string extra = "";
-//
-//	for (int i = 0; i < 200; i++)
-//	{
-//		inFS >> a >> b >> extra;
-//		if (!inFS.fail())
-//		{
-//			g.addEdge(a, b);
-//		}
-//	}
-//	inFS.close();
-//
-//	g.printAdjList();
-//
-//	return 0;
-//}
