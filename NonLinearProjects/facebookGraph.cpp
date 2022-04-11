02c0695ac97d66f4a4007d86441c00e10813b9c9
SplatterMelon <splattermelon@gmail.com>
2022-04-11 19:07:36 -0400

    hw10 progress

--- NonLinearProjects/facebookGraph.cpp
+++ NonLinearProjects/facebookGraph.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+﻿#include <vector>
 #include <unordered_map>
 #include <string>
 #include <fstream>
@@ -28,7 +28,7 @@ int main()
     int a = 0;
     int b = 0;
     string extra = "";
-    
+
     while (!inFS.eof())
     {
     	inFS >> a >> b >> extra;
@@ -42,6 +42,7 @@ int main()
 
     inFS.close();
 
+    cout << "hello" << endl;
     printList(uMap);
 
     return 0;
@@ -49,26 +50,36 @@ int main()
 
 void printList(unordered_map<int, vector<int>> const& m)
 {
-    for (auto const& pair : m) {
-        int i = 0;
-        cout << "{" << pair.first << ": ";
-        for (i = 0; i < pair.second.size() - 1; i++)
+    int num100plus = 0;
+    int top = 0;
+    int bot = 0;
+
+    for (auto const& pair : m)
+    {
+        bot++;
+        top += pair.second.size();
+
+        if (pair.second.size() >= 100)
         {
-            cout << pair.second.at(i) << ", ";
+            num100plus++;
         }
-        cout << pair.second.at(i) << "}\n";
     }
+
+    cout << "The average degree over all vertices in the graph is " << (top / bot) << "\nThe number of vertices with degree greater than or equal to 100 is " << num100plus;
 }
 
 void addEdge(int vertex1, int vertex2)
 {
-    if (find(edgeValues.begin(), edgeValues.end(), to_string(vertex1) + ":" + (to_string(vertex2))) == edgeValues.end() && find(edgeValues.begin(), edgeValues.end(), to_string(vertex2) + ":" + (to_string(vertex1))) == edgeValues.end())
+    /*if (find(edgeValues.begin(), edgeValues.end(), to_string(vertex1) + ":" + (to_string(vertex2))) == edgeValues.end() && find(edgeValues.begin(), edgeValues.end(), to_string(vertex2) + ":" + (to_string(vertex1))) == edgeValues.end())
     {
         uMap[vertex1].push_back(vertex2);
         uMap[vertex2].push_back(vertex1);
         edgeValues.push_back(to_string(vertex1) + ":" + (to_string(vertex2)));
         edgeValues.push_back(to_string(vertex2) + ":" + (to_string(vertex1)));
-    }
+    }*/
+
+    uMap[vertex1].push_back(vertex2);
+    uMap[vertex2].push_back(vertex1);
 }
 
 void addNewVertex(int vertex)
